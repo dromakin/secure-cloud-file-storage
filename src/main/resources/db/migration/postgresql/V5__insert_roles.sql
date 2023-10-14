db3f33d9e9f6dd167a9b0d32d32ed26ed2056342
Dmitriy Romakin <32188375+dromakin@users.noreply.github.com>
2023-10-14 18:15:27 +0300

    fix roles

--- src/main/resources/db/migration/postgresql/V5__insert_roles.sql
+++ src/main/resources/db/migration/postgresql/V5__insert_roles.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.roles(
   id, name
 ) VALUES
-    (1, 'USER'),
-    (2, 'WRITER'),
-    (3, 'ADMIN');
+    (1, 'ROLE_USER'),
+    (2, 'ROLE_WRITER'),
+    (3, 'ROLE_ADMIN');
--- src/main/resources/db/migration/postgresql/V6__insert_users.sql
+++ src/main/resources/db/migration/postgresql/V6__insert_users.sql
@@ -11,6 +11,6 @@ INSERT INTO public.users(
     status
 ) VALUES
     --admin/admin
-    (1, 'admin', '$2a$04$.09kYd8b8jvedKVFrSrvRuyCVILYwc/.NVZ3QvxGXYvzoq5PEo3PC', 'admin', 'admin', 'admin@localhost', 1, NOW(), NOW(), 'ACTIVE'),
-    (2, 'test', '$2a$04$.09kYd8b8jvedKVFrSrvRuyCVILYwc/.NVZ3QvxGXYvzoq5PEo3PC', 'test', 'test', 'test@localhost', 1, NOW(), NOW(), 'ACTIVE'),
-    (3, 'writer', '$2a$04$.09kYd8b8jvedKVFrSrvRuyCVILYwc/.NVZ3QvxGXYvzoq5PEo3PC', 'writer', 'writer', 'writer@localhost', 1, NOW(), NOW(), 'ACTIVE');
+    (1, 'admin@localhost', '$2a$04$.09kYd8b8jvedKVFrSrvRuyCVILYwc/.NVZ3QvxGXYvzoq5PEo3PC', 'admin', 'admin', 'admin@localhost', 1, NOW(), NOW(), 'ENABLE'),
+    (2, 'test@localhost', '$2a$04$.09kYd8b8jvedKVFrSrvRuyCVILYwc/.NVZ3QvxGXYvzoq5PEo3PC', 'test', 'test', 'test@localhost', 1, NOW(), NOW(), 'ENABLE'),
+    (3, 'writer@localhost', '$2a$04$.09kYd8b8jvedKVFrSrvRuyCVILYwc/.NVZ3QvxGXYvzoq5PEo3PC', 'writer', 'writer', 'writer@localhost', 1, NOW(), NOW(), 'ENABLE');
